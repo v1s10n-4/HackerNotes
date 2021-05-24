3902d04be5d227c45812c33fe5c86cbf07cae861
v1s10n_4 <romain.daniel@epitech.eu>
2021-05-24 21:33:46 +0200

    feat(navigation): add keyboard/mouse navigation with reakeys

--- src/pages/Terminal.tsx
+++ src/pages/Terminal.tsx
@@ -1,43 +1,95 @@
 import * as React from 'react';
-import { AppBar, Box, Button, makeStyles, Typography } from '@material-ui/core';
+import { AppBar, Box, Button, Dialog, makeStyles, Typography } from '@material-ui/core';
 import { useSelector } from 'react-redux';
 import { RootState } from '../reducers';
 import Editor from '../design-system/Components/Editor/Editor';
 import { useActions } from '../actions';
-import { Fragment, useCallback } from 'react';
+import { Fragment, useCallback, useState } from 'react';
 import * as NoteActions from '../actions/note';
-import { Toolbar } from '@material-ui/core';
+import { useHotkeys } from 'reakeys';
+import EditorHelpDialog from '../design-system/Components/EditorHelpDialog/EditorHelpDialog';
+import { useRouteParams } from 'react-typesafe-routes';
+import { router } from '../Router';
+import { useHistory } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
+	'@global': {
+		'*::selection': {
+			color: theme.palette.background.default,
+			backgroundColor: theme.palette.primary.main,
+		},
+	},
 	appBar: {
-		top: 'auto',
-		bottom: 0,
 		backgroundColor: theme.palette.background.default,
-		padding: 10,
+		border: 'none',
+
+		borderTop: `3px solid ${theme.palette.primary.main}`,
+		zIndex: 0,
+		padding: '5px 15px',
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'space-between',
 	},
 }));
 
-export function Terminal() {
+export const Terminal = () => {
 	const classes = useStyles();
-	const lastNote = useSelector((state: RootState) => state.noteList[0]);
-	const currentNote = JSON.parse(lastNote.text);
+	const history = useHistory();
 	const noteActions = useActions(NoteActions);
+
+	const { id } = useRouteParams(router.note);
+	const lastNote = useSelector((state: RootState) => state.noteList.find((x) => x.id == id));
+	const currentNoteValue = lastNote && JSON.parse(lastNote.text);
+
+	const [helpDialogOpen, setHelpDialogOpen] = useState(false);
+	const toggleHelpDialogOpen = useCallback(() => setHelpDialogOpen((prev) => !prev), []);
+
 	const onChange = useCallback(
 		(note) => {
-			noteActions.editNote({ id: lastNote.id, text: JSON.stringify(note) });
+			if (lastNote) noteActions.editNote({ id: lastNote.id, text: JSON.stringify(note) });
 		},
-		[noteActions, currentNote]
+		[noteActions, currentNoteValue]
 	);
+
+	console.log(id);
+
+	const goToList = useCallback(() => history.push(router.list().$), []);
+
+	const hotKeys = useHotkeys([
+		{
+			name: 'Save and exit',
+			description: 'Save you current note then redirect to notes list',
+			keys: 'esc',
+			callback: goToList,
+		},
+		{
+			name: 'Help',
+			description: 'Display this help message',
+			keys: 'mod+/',
+			callback: toggleHelpDialogOpen,
+		},
+	]);
+
 	return (
 		<Fragment>
-			{/*<Box width={'100%'} display={'flex'} justifyContent={'space-between'}>*/}
-			{/*	<Typography variant="h4">You have {todoList.length} Note!</Typography>*/}
-			{/*	<Button>create note</Button>*/}
-			{/*</Box>*/}
-			<Editor initialValue={currentNote} onChange={onChange} />
-			<AppBar variant={'outlined'} position={'fixed'} className={classes.appBar} component={'footer'}>
-				<Button>new note</Button>
+			<Editor initialValue={currentNoteValue} onChange={onChange} />
+			<AppBar variant={'outlined'} position={'relative'} className={classes.appBar} component={'footer'}>
+				<Typography color={'textPrimary'} variant={'caption'}>
+					Press &lt;Esc&gt; to{' '}
+					<Button variant={'text'} onClick={goToList}>
+						save & exit
+					</Button>
+				</Typography>
+				<Typography color={'textPrimary'} variant={'caption'}>
+					Press &lt;cmd&gt;+&lt;/&gt; for
+					<Button variant={'text'} onClick={toggleHelpDialogOpen}>
+						help
+					</Button>
+				</Typography>
 			</AppBar>
+			<EditorHelpDialog open={helpDialogOpen} onClose={toggleHelpDialogOpen} />
 		</Fragment>
 	);
-}
+};
+
+export default Terminal;
