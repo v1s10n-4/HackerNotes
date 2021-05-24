047d526c91b0e8fd99b9f9aa53b17edc40b0de14
v1s10n_4 <romain.daniel@epitech.eu>
2021-05-24 21:36:24 +0200

    chore(routes): change routes to allow /list -> /note + some styles improvements

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,4 @@
-export * from "./Terminal";
-export * from "./HomePage";
-export * from "./TodoPage";
+export * from './Terminal';
+export * from './HomePage';
+export * from './TodoPage';
+export * from './NoteList';
