cc62e1cd9f05e8e94eedcc08215e1d16a3dc583e
v1s10n_4 <romain.daniel@epitech.eu>
2021-05-25 22:16:47 +0200

    chore: delete sample code from react-factory

--- src/model/index.ts
+++ src/model/index.ts
@@ -1,11 +1,7 @@
-import { TodoAction } from './todo';
 import { ConfigAction } from './config';
 import { NoteAction } from './note';
 
 export * from './config';
-export * from './todo';
 export * from './note';
 
-
-export type Action =
-    | ConfigAction | TodoAction | NoteAction;
+export type Action = ConfigAction | NoteAction;
